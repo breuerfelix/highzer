db1bbe975ce68396ded4762b9903ad407fe24f3c
Felix Breuer <f.breuer94@gmail.com>
2021-01-09 21:46:41 +0100

    a note on the client id
    Signed-off-by: Felix Breuer <f.breuer94@gmail.com>

--- highzer/config.py
+++ highzer/config.py
@@ -1,1 +1,4 @@
+# this is the official Client-ID which is being used
+# by everyone who is not logged in to twitch.tv
+# while using the website, no need to worry that i put it here (:
 CLIENT_ID = "kimne78kx3ncx6brgo4mv6wki5h1ko"
