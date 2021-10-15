4e0caf7c7fb0b4c19ce73a99d43c457f1867d1c7
Felix Breuer <fbreuer@pm.me>
2021-10-15 14:47:36 +0200

    upload lib
    Signed-off-by: Felix Breuer <fbreuer@pm.me>

---
+++ shell.nix
@@ -0,0 +1,17 @@
+{ pkgs ? import <nixpkgs> {} }:
+let
+  highzer = pkgs.poetry2nix.mkPoetryEnv {
+    projectDir = ./.;
+    editablePackageSources = {
+      highzer = ./highzer;
+    };
+  };
+in highzer.env.overrideAttrs (oldAttrs: {
+  buildInputs = with pkgs; [
+    python3
+    poetry
+    geckodriver
+    ffmpeg
+    imagemagick
+  ];
+})
